93721b8cb9d299b32871e1470ac2f6885083b4a9
vaibhav26-2007 <72144239+vaibhav26-2007@users.noreply.github.com>
2020-11-30 11:17:28 +0530

    Add files via upload

---
+++ Take A Break Notification Program.py
@@ -0,0 +1,19 @@
+from plyer import notification
+import time
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "Take A Break",
+            message = "Take A Break\n"
+                      "Take Rest For Your Eyes And Your Body\n"
+                      "Go Walk",
+            timeout = 7
+
+
+
+
+        )
+        time.sleep(60*30)
+
+
